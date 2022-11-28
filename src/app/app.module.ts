abc3e18fe6f3cdd40566d4e0bec572fcfb2986e4
Jader Andres Gaviria Betancur <jagaviriab@siesa.com>
2022-11-27 23:38:29 -0500

    Segundo Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { YouTubePlayerModule } from "@angular/youtube-player";
 
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { RouterModule, Routes } from '@angular/router';
+import { QuienessomosComponent } from './components/quienessomos/quienessomos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { TestimoniosComponent } from './components/testimonios/testimonios.component';
+import { ContactanosComponent } from './components/contactanos/contactanos.component';
+import { HomeComponent } from './components/home/home.component';
+import { AsideComponent } from './components/aside/aside.component';
+
+
+//Rutas
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'quienessomos', component: QuienessomosComponent},
+  {path: 'servicios', component: ServiciosComponent},
+  {path: 'testimonios', component: TestimoniosComponent },
+  {path: 'contactanos', component: ContactanosComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    InicioComponent,
+    QuienessomosComponent,
+    ServiciosComponent,
+    TestimoniosComponent,
+    ContactanosComponent,
+    HomeComponent,
+    AsideComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    YouTubePlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+nombre = 'TML Agencia de publicidad - publicidad@tml.org.co -';
+desarrollo = 'Jader Andrés Gaviria Betancur'
+}
